6b716935a2f38ddccb8f1f8640a50e5b377cdbf3
Борис Чередниченко <cherednychenko.borys@gmail.com>
2017-05-20 19:08:17 +0300

    клик по форме

--- diplomaPMA/Form1.Designer.cs
+++ diplomaPMA/Form1.Designer.cs
@@ -28,33 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 67);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "я ахуевший вау";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Days", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(176, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ХУЙ СОСИ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(748, 539);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(24, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Exit";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- diplomaPMA/Form1.cs
+++ diplomaPMA/Form1.cs
@@ -12,9 +12,30 @@ namespace diplomaPMA
 {
     public partial class Form1 : Form
     {
+        private void MyForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button btn = new Button
+            {
+                Location = e.Location, // e.Location - координаты мыши
+                Visible = true,
+                Text = "Some text"
+            };
+            Controls.Add(btn);
+        }
         public Form1()
         {
             InitializeComponent();
+            MouseClick += MyForm_MouseClick;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
